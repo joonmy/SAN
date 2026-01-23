7fdeef55c8e5b9bb8f412131443803222ee8ebbc
joonmy <joonmy>
2026-01-23 14:15:47 +0900

    fix the code error

--- train.bash
+++ train.bash
@@ -11,5 +11,5 @@ python -m torch.distributed.launch \
     --lr 0.01 \
     --loss_lambda 0.4 \
     --num_hard 5 \
-    --neg_table_name /home/user/jmlee/final_exp/phoenix_batch32_half_frame_cico/hard_neg_pkl/SAN/train/train_hard_word_777.pkl.gz \
-    --output_dir /home/user/jmlee/out/ablation/test
+    --neg_table_name "path to neg table" \
+    --output_dir "path to output"
